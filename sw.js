50535cf52e6406b052b0570722a452e8a6d04018
samueldeazevedo1981-creator <samueldeazevedo1981@gmail.com>
2026-09-02 14:55:43 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "trucker-cap-player-video-16x9-v34";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon.png", "./icon-192.png", "./soundtouch-node.js", "./constants.js", "./soundtouch-processor.js", "./SOUNDTOUCH-LICENSE.txt"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.protocol !== "http:" && url.protocol !== "https:") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
